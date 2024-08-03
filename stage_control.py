1ff4d49dcb2f91d511d78de467bfc174b4f2f047
Logan Walker <loganaw@umich.edu>
2024-08-03 19:30:59 -0400

    Update stage_control.py

--- stage_control.py
+++ stage_control.py
@@ -96,6 +96,99 @@ class ESP302StageControl:
         }
 
 
+class ESP300StageControl:
+    def __init__(self, serial_name: str, serial_baud: int, timeout: int):
+        self._serial = serial.Serial(serial_name, serial_baud, timeout=timeout)
+        self._serial_mutex = threading.Lock()
+
+    def __del__(self):
+        self.close()
+
+    def __repr__(self):
+        return (
+            "ESPStageControl(connected on "
+            + repr(self._serial)
+            + ") -> "
+            + repr(self.status())
+        )
+
+    def close(self) -> None:
+        with self._serial_mutex:
+            self._serial.close()
+
+    def home(self, axis: int) -> None:
+        cmd = f"{axis}OR\n"
+        with self._serial_mutex:
+            self._serial.write(bytes(cmd, "utf-8"))
+
+    def home_all(self) -> None:
+        with self._serial_mutex:
+            self._serial.write(b"0OR\n")
+
+    def get_current_position(self) -> dict:
+        with self._serial_mutex:
+            self._serial.write(b"TP\n")
+            rv = self._serial.readline().strip().split(b",")
+            return {i + 1: float(x) for i, x in enumerate(rv)}
+
+    def emergency_stop(self, axis: int) -> None:
+        with self._serial_mutex:
+            self._serial.write(b"AB\n")
+
+    def stop(self, axis: int) -> None:
+        cmd = f"{axis}ST\n"
+        with self._serial_mutex:
+            self._serial.write(bytes(cmd, "utf-8"))
+
+    def get_is_moving(self) -> dict:
+        with self._serial_mutex:
+            self._serial.write(b"TS\n")
+            rv = self._serial.readline().strip()  # get result from stage
+            rv = bin(rv[0])[2:][::-1][:3]
+            return {i + 1: (v == "1") for i, v in enumerate(rv)}  # check order
+
+    def is_moving(self) -> bool:
+        return any(self.get_is_moving().values())
+
+    def wait_for_move(self) -> None:
+        while self.is_moving():
+            pass  # maybe replace with less busy wait
+
+    def send_move(self, axis: int, position: float) -> None:
+        cmd = f"WT50\n{axis}PA{position}\n"
+        self.stop(axis)
+        with self._serial_mutex:
+            self._serial.write(bytes(cmd, "utf-8"))
+
+    def send_velocity(self, axis: int, velocity: float) -> None:
+        cmd = f"{axis}VA{velocity}\n"
+        with self._serial_mutex:
+            self._serial.write(bytes(cmd, "utf-8"))
+
+    def send_move_indefinite(self, axis: int, dir: str) -> None:
+        if not ((dir == "+") or (dir == "-")):
+            return
+        cmd = f"{axis}MV{dir}\n"
+        with self._serial_mutex:
+            self._serial.write(bytes(cmd, "utf-8"))
+
+    def send_move_wait(self, axis: int, position: float) -> None:
+        self.send_move(axis, position)
+        self.wait_for_move()
+
+    def send_enable_axis(self, axis: int) -> None:
+        cmd = f"{axis}MO\n"
+        with self._serial_mutex:
+            self._serial.write(bytes(cmd, "utf-8"))
+
+    def status(self) -> dict:
+        return {
+            "position": self.get_current_position(),
+            "axes_moving": self.get_is_moving(),
+            "stage_active_flag": self.is_moving(),
+        }
+
+
 class TriggerControl:
     def __init__(self, serial_name: str, serial_baud: int, timeout: int):
         self._serial = serial.Serial(serial_name, serial_baud, timeout=timeout)
